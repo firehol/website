dd5eff32b35d89ab4f539076ddf85a16f4c7f153
Phil Whineray <phil@sanewall.org>
2013-12-08 10:19:25 +0000

    Make overview the front page
    Include latest news item at top of the overview page
    
    Use an example domain that exists and will resolve, include the ipv4
    keyword in preparation for v2.0.0
    
    Link to the FireQOS tutorial from its news item

--- lib/analytics.rb
+++ lib/analytics.rb
@@ -1,12 +1,15 @@
 module AnalyticsHelper
 
+#  def ga_link(id,url,name)
+#   "<a id=\"#{id}\" href=\"#{url}\">#{name}</a>" +
+#   "<script>" +
+#   "$('##{id}').on('click', function() {" +
+#   " ga('send', { 'hitType': 'pageview', 'page': '#{url}' });" +
+#   " });" +
+#   "</script>"
+#  end
   def ga_link(id,url,name)
-   "<a id=\"#{id}\" href=\"#{url}\">#{name}</a>" +
-   "<script>" +
-   "$('##{id}').on('click', function() {" +
-   " ga('send', { 'hitType': 'pageview', 'page': '#{url}' });" +
-   " });" +
-   "</script>"
+   "<a id=\"#{id}\" href=\"#{url}\">#{name}</a>"
   end
 
 end
