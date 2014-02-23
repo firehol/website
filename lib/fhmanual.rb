6ec48c47b68e492338f39c6c27ff577922855579
Phil Whineray <phil@sanewall.org>
2014-02-23 20:44:42 +0000

    Replace example formatter
    Replace many-regexp per line version with a much more efficient and
    less error prone tokeniser and hash-based lookup of keywords.

--- lib/fhmanual.rb
+++ lib/fhmanual.rb
@@ -1,4 +1,6 @@
 module FireholManualHelper
+  require 'strscan'
+
   def fhmanual_prepare(manual,services)
     fhmanual_prepare_int(manual)
     fhmanual_prepare_services_int(services)
@@ -19,26 +21,37 @@ module FireholManualHelper
     end
 
     if example[:keywords] == "hol"
-      params = [ :fhmanual_hol, :fhmanual_services ]
+      sections = [ @config[:fhmanual_hol], @config[:fhmanual_services] ]
     elsif example[:keywords] == "qos"
-      params = [ :fhmanual_qos ]
+      sections = [ @config[:fhmanual_qos] ]
     else
       raise "example #{name} has unknown keyword type '#{example[:keywords]}'"
     end
 
     "<pre class='programlisting'>\n" +
-        fhmanual_fixup(example.raw_content, *params) +
+        fhmanual_fixup(example.raw_content, sections) +
     "\n</pre>"
   end
 
   def fhmanual_services_by_alpha(letter)
-    keys = @config[:fhmanual_service_list].keys
+    keys = @config[:fhmanual_services].keys
     matched = keys.select {|i| i.upcase.match("^#{letter.upcase}") }
     return matched.sort { |a,b| a.upcase <=> b.upcase }
   end
 
   ###################### Internal ######################
   private
+
+  class ManualReference
+    attr_accessor :keyword
+    attr_accessor :url
+    attr_accessor :anchor
+    attr_accessor :special
+
+    def special?
+      @special != nil
+    end
+  end
 
   def fhmanual_ref(id, text, url = nil)
      if url == nil
@@ -60,23 +73,23 @@ module FireholManualHelper
   def fhmanual_special_r(word, id_if_first, id_if_not_first)
     # These special cases are keywords that mean something different
     # at the beginning of a configuration line than elsewhere in the line
-    h = Hash.new
-    h[/^( *)#{word}( )/] = "\\1#{fhmanual_ref(id_if_first, word)}\\2"
-    h[/([^ ] )#{word}( *)/] = "\\1#{fhmanual_ref(id_if_not_first, word)}\\2"
-    h
+    manref = ManualReference.new
+    manref.keyword = word
+    manref.special = [ fhmanual_ref(id_if_first, word), fhmanual_ref(id_if_not_first, word) ]
+    manref
+  end
+
+  def fhmanual_special_x(word)
+    manref = ManualReference.new
+    manref.keyword = word
+    manref.special = [ ]
+    manref
   end
 
   def fhmanual_prepare_int(uri)
     puts "Loading example code => uri data"
     current_html_anchor = nil
-    special = Hash.new
-    special['mac']  = fhmanual_special_r('mac','helpconf-mac','rule-params')
-    special['dscp'] = fhmanual_special_r('dscp','helpconf-dscp','rule-params')
-    special['mark'] = fhmanual_special_r('mark','helpconf-mark','rule-params')
-    special['tos']  = fhmanual_special_r('tos','helpconf-tos','rule-params')
 
-    qspecial = Hash.new
-    qspecial['class']=fhmanual_special_r('class','qos-class','qos-match-params')
 #    qspecial['prio'] = {
 #     /^( *)class( )/ => "\\1#{fhmanual_ref('qos-class-params', 'prio')}\\2",
 #     /^( *)match( )/ => "\\1#{fhmanual_ref('qos-match-params', 'prio')}\\2"
@@ -85,21 +98,24 @@ module FireholManualHelper
 #     /^( *)class( )/ => "\\1#{fhmanual_ref('qos-class-params', 'priority')}\\2",
 #     /^( *)match( )/ => "\\1#{fhmanual_ref('qos-match-params', 'priority')}\\2"
 #                      }
-    qspecial['prio'] = {}
-    qspecial['priority'] = {}
-    hol_data = Hash.new
+    hol_data = nil
     manual_data = Hash.new
-    ids_seen = Hash.new
+    manual_data['mac'] =fhmanual_special_r('mac','helpconf-mac','rule-params')
+    manual_data['dscp']=fhmanual_special_r('dscp','helpconf-dscp','rule-params')
+    manual_data['mark']=fhmanual_special_r('mark','helpconf-mark','rule-params')
+    manual_data['tos'] =fhmanual_special_r('tos','helpconf-tos','rule-params')
     all_anchors = Hash.new
     errors = nil
     open(uri, "rb") do |infile|
       infile.each_line do |line|
         line.gsub(/</, "\n<").split(/\n/).each do |real_line|
           if real_line.match(/class="part" title=".*FireQOS Reference"/)
-            special = qspecial
             hol_data = manual_data
             manual_data = Hash.new
-            ids_seen = Hash.new
+            manual_data['class'] = fhmanual_special_r('class','qos-class',
+                                                      'qos-match-params')
+            manual_data['prio'] = fhmanual_special_x('prio')
+            manual_data['priority'] =fhmanual_special_x('priority')
           end
 
           if m = real_line.match(/<a name="([^"]+)">/)
@@ -108,26 +124,20 @@ module FireholManualHelper
           end
           if m = real_line.match(/<code class="command">(.*)/)
             m[1].gsub(/ +or +/, "|").split(/\|/).each do |keyword|
-            next if keyword == ""
-            if special[keyword]
-              special[keyword].each do |k,v|
-                manual_data[k] = v
-              end
-              ids_seen[keyword] = current_html_anchor
-            else
-              link = fhmanual_ref(current_html_anchor, keyword)
-              if ids_seen[keyword] != nil and ids_seen[keyword] != current_html_anchor
-                puts "'#{keyword}' in examples is both '#{ids_seen[keyword]}' and '#{current_html_anchor}', define a special"
-                errors = true
+              next if keyword == ""
+              if manual_data[keyword]
+                if not manual_data[keyword].special? and
+                       manual_data[keyword].anchor != current_html_anchor
+                  puts "'#{keyword}' in examples is both '#{manual_data[keyword].anchor}' and '#{current_html_anchor}', define a special"
+                  errors = true
+                end
               else
-                #puts "'#{keyword}' in examples is '#{current_html_anchor}'"
-                ids_seen[keyword] = current_html_anchor
-                manual_data[/^( *)#{keyword}( )/] = "\\1#{link}\\2"
-                manual_data[/([ >])#{keyword}([ <]*)$/] = "\\1#{link}\\2"
-                manual_data[/([ >])#{keyword}([ <])/] = "\\1#{link}\\2"
-                manual_data[/^()#{keyword}()$/] = "\\1#{link}\\2"
+                manref = ManualReference.new
+                manref.keyword = keyword
+                manref.url = fhmanual_ref(current_html_anchor, keyword)
+                manref.anchor = current_html_anchor
+                manual_data[keyword] = manref
               end
-            end
             end
           end
         end
@@ -146,29 +156,29 @@ module FireholManualHelper
     puts "Loading example services => uri data"
     current_html_anchor = nil
     manual_data = Hash.new
-    ids_seen = Hash.new
     errors = nil
     open(uri, "rb") do |infile|
       infile.each_line do |line|
         if m = line.match(/<a href=".*#(service-.*)">(.*)</)
           current_html_anchor = m[1]
-          link = fhmanual_ref(current_html_anchor, m[2])
-          if ids_seen[m[1]] != nil and ids_seen[m[1]] != current_html_anchor
-            puts "'#{m[1]}' in examples is both '#{ids_seen[m[1]]}' and '#{current_html_anchor}', define a special"
-            errors = true
+          keyword = m[2]
+          if manual_data[keyword]
+            if manual_data[keyword].anchor != current_html_anchor
+              puts "'#{keyword}' in examples is both '#{manual_data[keyword].anchor}' and '#{current_html_anchor}', define a special"
+              errors = true
+            end
           else
-            ids_seen[m[2]] = current_html_anchor
-            manual_data[/^( *)#{m[2]}( )/] = "\\1#{link}\\2"
-            manual_data[/([ >"])#{m[2]}([ <"]*)$/] = "\\1#{link}\\2"
-            manual_data[/([ >"])#{m[2]}([ <"])/] = "\\1#{link}\\2"
-            manual_data[/^()#{m[2]}()$/] = "\\1#{link}\\2"
+            manref = ManualReference.new
+            manref.keyword = keyword
+            manref.url = fhmanual_ref(current_html_anchor, keyword)
+            manref.anchor = current_html_anchor
+            manual_data[keyword] = manref
           end
         end
       end
     end
     raise "Errors preparing example services" if errors
     @config[:fhmanual_services] = manual_data
-    @config[:fhmanual_service_list] = ids_seen
   end
 
   def fhmanuals
@@ -181,33 +191,52 @@ module FireholManualHelper
     l.shift
   end
 
-  def fhmanual_fixup(text, *fixup_from)
+  def fhmanual_fixup(text, sections)
+    outtext = ""
     lines = text.split("\n").collect do |line|
-      line.sub!(/#([^"']*)$/, '<span class="comment">#\1</span>')
-      line.sub!(/^( *)(<span class="[^"]*">)?( *)([^ =<>]*)=/, '\2<span class="var">\1\3\4</span>=')
-      line.gsub!(/(\$[^ "]*)/, '<span class="var">\1</span>')
-      fixup_from.each do |replace_name|
-        line.gsub!(/<span class/, "<spanclass") # Prevent sub as keyword class
-        front=nil
-        if m = line.match(/^( *<span[^>]+> *)(.+)/)
-          front = m[1]
-          line = m[2]
-        end
-        keys=@config[replace_name].keys.sort {|a,b|b.to_s.length-a.to_s.length}
-        keys.each do |k|
-          if replace_name.to_s.match(/qos/) and k.to_s.match(/class/)
+      first_word = true
+      ss = StringScanner.new(line)
+      loop do
+        is_word = false
+        word = if ss.peek(1) == "<"
+                 ss.scan(/<[^>]*>/)
+               elsif ss.peek(1) == "#"
+                 w = ss.scan(/.*/)
+                 '<span class="comment">' + w + '</span>'
+               elsif ss.peek(1) == "$"
+                 w = ss.scan(/\${?[A-Za-z0-9_]+}?/)
+                 '<span class="var">' + w + '</span>'
+               elsif ss.match?(/\s+/)
+                 ss.scan(/\s+/)
+               elsif ss.match?(/\w+=/)
+                 w = ss.scan(/\w+/)
+                 ss.getch
+                 '<span class="var">' + w + '</span>='
+               elsif ss.match?(/[\w&+-.]+/)
+                 is_word = true
+                 ss.scan(/[\w&+-.]+/)
+               else
+                 ss.getch
+               end
+        break if word.nil?
+        sections.each do |manrefs|
+          manref = manrefs[word]
+          if manref and manref.special and manref.special.size > 1
+            if first_word
+              word = manref.special[0].to_s
+            else
+              word = manref.special[1].to_s
+            end
+          elsif manref and manref.url
+            word = manref.url
           end
-          line.gsub!(k, @config[replace_name][k]);
         end
-        if front
-          line = front + line
-        end
+        outtext += word
+        first_word = false if is_word
       end
-      line.gsub!(/<spanclass/, "<span class")
-      line
+      outtext += "\n"
     end
-    GC.start
-    lines.join("\n")
+    outtext
   end
 
   def fhmanual_anchor(anchor)
