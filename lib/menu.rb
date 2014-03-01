e73bf34925f7c05a8303725ae902c59f6bfde8ed
Phil Whineray <phil@sanewall.org>
2014-03-01 08:57:16 +0000

    Split menu links and expanders
    Will allow introduction of top-level descriptions

--- lib/menu.rb
+++ lib/menu.rb
@@ -7,6 +7,14 @@ module MenuHelper
          "closedbydefault"
      end
   end
+
+  def submenu_ind(menu, item_belongs_in)
+     if submenu_class(menu, item_belongs_in) == "openbydefault"
+       "<a class=\"toggle\" name=\"#{menu}\">[-]</a>"
+     else
+       "<a class=\"toggle\" name=\"#{menu}\">[+]</a>"
+     end
+  end
 end
 
 include MenuHelper
